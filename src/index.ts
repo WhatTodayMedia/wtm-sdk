2edbc83cf6f38a7fb013ae096b250d2c493f786c
yoohwanbin <hwanbin@infludeo.com>
2024-05-08 17:22:32 +0900

    Update : setting (gitignore, ts, dist)

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+function add(a: any, b: any) {
+  return a + b;
+}
+function subtract(a: number, b: number) {
+  return a - b;
+}
+function multiply(a: number, b: number) {
+  return a * b;
+}
+function divide(a: number, b: number) {
+  return a / b;
+}
+
+export { add, subtract, multiply, divide };

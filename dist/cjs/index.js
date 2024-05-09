a833e2bb2544f86f014037f68dbe2b1cb94f5839
yoohwanbin <hwanbin@infludeo.com>
2024-05-09 10:09:54 +0900

    Add : esm, ts, cjs setting

---
+++ dist/cjs/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.divide = exports.multiply = exports.subtract = exports.add = void 0;
+function add(a, b) {
+    return a + b;
+}
+exports.add = add;
+function subtract(a, b) {
+    return a - b;
+}
+exports.subtract = subtract;
+function multiply(a, b) {
+    return a * b;
+}
+exports.multiply = multiply;
+function divide(a, b) {
+    return a / b;
+}
+exports.divide = divide;

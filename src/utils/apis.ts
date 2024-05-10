5a093afda33a0fab4f0e416969189d1326006e69
yoohwanbin <hwanbin@infludeo.com>
2024-05-10 10:46:02 +0900

    Add : firebase, api, list type

---
+++ src/utils/apis.ts
@@ -0,0 +1,33 @@
+import { collection, getDocs, query, orderBy, where } from "firebase/firestore";
+import { db } from "./fbase";
+
+export const getWtmData = async () => {
+  const arr: any = [];
+  const q = query(collection(db, "media"));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => arr.push(doc.data()));
+  return arr;
+};
+
+export const getWtmCategoryData = async (
+  category: string,
+  sort?: "asc" | "desc"
+) => {
+  const arr: any = [];
+  const q = query(collection(db, "media"), orderBy(category, sort));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => arr.push(doc.data()));
+  return arr;
+};
+
+export const getWtmSearchData = async (category: string, keyword: string) => {
+  const arr: any = [];
+  const q = query(
+    collection(db, "media"),
+    where(category, ">=", keyword),
+    where(category, "<=", keyword + "\uf8ff")
+  );
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => arr.push(doc.data()));
+  return arr;
+};

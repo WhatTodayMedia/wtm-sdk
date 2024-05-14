c753a04bd4dc5c1e7a31f4689bfd6f9ae0ee9d67
yoohwanbin <hwanbin@infludeo.com>
2024-05-14 16:59:15 +0900

    Update : build files

--- dist/apis/get.d.ts
+++ dist/apis/get.d.ts
@@ -1,4 +1,4 @@
 export declare const getWtmData: () => Promise<any>;
-export declare const getWtmCategoryData: (category: string, sort?: "asc" | "desc") => Promise<any>;
-export declare const getWtmSearchData: (category: string, keyword: string) => Promise<any>;
+export declare const getWtmFilterData: (filterName: string, sort?: "asc" | "desc") => Promise<any>;
+export declare const getWtmSearchData: (keyword: string, category?: string) => Promise<any>;
 export declare const getWtmDetailData: (subDocument: string, subCollection: string) => Promise<any>;
--- dist/esm/apis/get.js
+++ dist/esm/apis/get.js
@@ -7,16 +7,16 @@ export const getWtmData = async () => {
     querySnapshot.forEach((doc) => arr.push(doc.data()));
     return arr;
 };
-export const getWtmCategoryData = async (category, sort) => {
+export const getWtmFilterData = async (filterName, sort) => {
     const arr = [];
-    const q = query(collection(db, "media"), orderBy(category, sort));
+    const q = query(collection(db, "media"), orderBy(filterName, sort));
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => arr.push(doc.data()));
     return arr;
 };
-export const getWtmSearchData = async (category, keyword) => {
+export const getWtmSearchData = async (keyword, category) => {
     const arr = [];
-    const q = query(collection(db, "media"), where(category, ">=", keyword), where(category, "<=", keyword + "\uf8ff"));
+    const q = query(collection(db, "media"), where(category ?? "title", ">=", keyword), where(category ?? "title", "<=", keyword + "\uf8ff"));
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => arr.push(doc.data()));
     return arr;

cf421336d96d62c11c2330f3445c87a61d330a5d
유환빈 <hwanbin@infludeo.com>
2024-05-16 16:35:15 +0900

    Add : Category type (#7)

--- src/apis/add.ts
+++ src/apis/add.ts
@@ -1,6 +1,6 @@
 import { doc, collection, setDoc } from "firebase/firestore";
 import { db } from "../utils/fbase";
-import { WtmListType } from "../types/list";
+import { WtmListType } from "../types";
 
 export const createWtmData = async (createObj: WtmListType) => {
   const docRef = doc(collection(db, "media"));
---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export interface WtmListType {
+  title: string;
+  described: string;
+  thumbnailUrl: string;
+  tag: "영화" | "드라마" | "예능";
+  category: CategoryType[];
+  grade: string;
+  recommended: boolean;
+  famousLine: string;
+  startDate: Date;
+  endDate?: Date | undefined;
+  ost: string;
+  trailer: string;
+  watchaPedia: string;
+}
+
+export type CategoryType =
+  | "휴먼"
+  | "로맨스"
+  | "액션 "
+  | "판타지"
+  | "범죄"
+  | "스릴러"
+  | "미스터리"
+  | "코미디"
+  | "느와르"
+  | "서바이벌"
+  | "호러"
+  | "SF";

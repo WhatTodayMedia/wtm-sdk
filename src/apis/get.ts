5a23d8a3a5aa610eccf54604f4a7985d7ac35c57
yoohwanbin <hwanbin@infludeo.com>
2024-05-23 15:49:14 +0900

    Fix : rollback

--- src/apis/get.ts
+++ src/apis/get.ts
@@ -3,15 +3,18 @@ import { db } from "../utils/fbase";
 import { TagType } from "..";
 
 export const getWtmData = async () => {
+  const arr: any = [];
   const q = query(collection(db, "media"));
   const querySnapshot = await getDocs(q);
-  return querySnapshot.forEach((doc) => doc.data());
+  querySnapshot.forEach((doc) => arr.push(doc.data()));
+  return arr;
 };
 
 export const getWtmFilterData = async (
   tagName: TagType,
   filterNames: string[]
 ) => {
+  const arr: any = [];
   const q = query(
     collection(db, "media"),
     where(
@@ -22,24 +25,29 @@ export const getWtmFilterData = async (
     where("category", "array-contains-any", filterNames)
   );
   const querySnapshot = await getDocs(q);
-  return querySnapshot.forEach((doc) => doc.data());
+  querySnapshot.forEach((doc) => arr.push(doc.data()));
+  return arr;
 };
 
 export const getWtmSearchData = async (keyword: string, category?: string) => {
+  const arr: any = [];
   const q = query(
     collection(db, "media"),
     where(category ?? "title", ">=", keyword),
     where(category ?? "title", "<=", keyword + "\uf8ff")
   );
   const querySnapshot = await getDocs(q);
-  return querySnapshot.forEach((doc) => doc.data());
+  querySnapshot.forEach((doc) => arr.push(doc.data()));
+  return arr;
 };
 
 export const getWtmDetailData = async (
   subDocument: string,
   subCollection: string
 ) => {
+  const arr: any = [];
   const q = query(collection(db, "media", subDocument, subCollection));
   const querySnapshot = await getDocs(q);
-  return querySnapshot.forEach((doc) => doc.data());
+  querySnapshot.forEach((doc) => arr.push(doc.data()));
+  return arr;
 };
